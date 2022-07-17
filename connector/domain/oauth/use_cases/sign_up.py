27994e5bbc945b107b1651f7367d45253cb7811d
leeleelee3264 <absinthe4902@naver.com>
2022-07-17 20:17:17 +0900

    Apply pipenv and lint checker

--- connector/domain/oauth/use_cases/sign_up.py
+++ connector/domain/oauth/use_cases/sign_up.py
@@ -4,8 +4,10 @@ from domain.oauth.services.sign_up import OauthSignupService as Service
 
 class SingpassSignUpUseCase:
 
-    def __init__(self, service: Service):
+    def __init__(self, service: Service) -> None:
         self._myinfo_service = service
 
     def get_authorise_url(self) -> MyinfoAuthoriseRedirectUrl:
         return self._myinfo_service.get_authorise_url()
+
+    # def get_person(self, ):
